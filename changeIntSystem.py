84ab85cfacceda1b7651318561630339d3c90ef8
towasi-dot <tomwasi@outlook.com>
2026-06-04 22:53:21 +0200

    Refactor binary conversion to support arbitrary bases

--- changeIntSystem.py
+++ changeIntSystem.py
@@ -1,4 +1,4 @@
-def to_binary(n):
+def to_system(sys: int, n: int) -> str:
     if n == 0:
         return "0"
 
@@ -7,21 +7,13 @@ def to_binary(n):
 
     result = ""
     while n > 0:
-        result += str(n % 2)
-        n //= 2
+        result += str(n % sys)
+        n //= sys
 
     return result[::-1]
 
-def to_ternary(n):
-    if n == 0:
-        return "0"
-
-    if n == 1:
-        return "1"
-
-    result = ""
-    while n > 0:
-        result += str(n % 3)
-        n //= 2
-
-    return result[::-1]
+def from_system(sys: int, n: str) -> int:
+    result = 0
+    for c in n:
+        result = result * sys + int(c)
+    return result
